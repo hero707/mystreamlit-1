7f8aebcbca6b12381f8734fc4b5aebaacf68994c
koyoungwoong <yuko@hallym.ac.kr>
2023-07-11 20:42:32 +0900

    v2

--- uber_pickups.py
+++ uber_pickups.py
@@ -16,7 +16,6 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
 
